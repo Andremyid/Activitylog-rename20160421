99f1fe45690363319a2fdd5384250032dfba249b
Andremyid <android@andre.my.id>
2016-04-17 03:20:49 +0700

    Activitylog Service Provider

---
+++ src/Andremyid/Activitylog/ActivitylogServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Andremyid\Activitylog;
+
+use Illuminate\Support\ServiceProvider;
+use Config;
+
+class ActivitylogServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('andremyid/activitylog');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind(
+			'activity',
+			'Andremyid\Activitylog\Activitylog'
+			);
+
+		$this->app->bind(
+			'Andremyid\Activitylog\Handlers\ActivitylogHandlerInterface',
+			'Andremyid\Activitylog\Handlers\EloquentHandler'
+			);
+
+		if (! empty(Config::get('activitylog.autoAliasLoader')) || ! empty(Config::get('activitylog::autoAliasLoader'))) {
+			if (Config::get('activitylog.autoAliasLoader') == true || Config::get('activitylog::autoAliasLoader') == true) {
+				$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+				$loader->alias('Activity', 'Andremyid\Activitylog\ActivitylogFacade');
+			}
+		}
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('activity');
+	}
+
+}
